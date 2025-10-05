b768eb4f6500e25e6ab1a0f06352b4a34fe5b05b
Viktor Andersen <viktoranders3n@gmail.com>
2025-10-05 10:59:21 +0200

    Add slim to project

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+
+gem "slim", "~> 5.2"
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
-
   def landing
   end
 
